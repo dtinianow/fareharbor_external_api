ef2b05f3c53c1ed1d08d1ba04c4a9c9ace722af5
David Tinianow <david.tinianow@gmail.com>
2016-09-18 15:31:14 -0600

    Add get items and get availabilities by date methods

--- fareharbor_external_api/fh.py
+++ fareharbor_external_api/fh.py
@@ -9,8 +9,25 @@ class Service:
     payload = {'api-app': api_app_key, 'api-user': api_user_key}
 
     def get_companies(self):
-        request = requests.get(self.url + 'companies/', params=self.payload)
+        request = requests.get('%scompanies/' % self.url, params=self.payload)
         print request.text
 
+    def get_items(self, shortname):
+        request = requests.get('%scompanies/%s/items/' % (self.url, shortname), params=self.payload)
+        print request.text
+
+    def get_availabilities_by_date(self, shortname, data):
+        request = requests.get('%scompanies/%s/items/%s/minimal/availabilities/date/%s/' % (self.url, shortname, data['pk'], data['date']), params=self.payload)
+        print request.text
+
+    def get_availabilities_by_date_range(self, shortname, data):
+        request = requests.get('%scompanies/%s/items/%s/minimal/availabilities/date-range/%s/%s/' % (self.url, shortname, data[:pk], data[:start_date], data[:end_date]), params=self.payload)
+        print request.text
+
+
 x = Service()
-x.get_companies()
+# x.get_companies()
+# x.get_items('bodyglove')
+data = {'pk': 1108, 'date': '2016-11-14'}
+# import code; code.interact(local=dict(globals(), **locals()))
+x.get_availabilities_by_date('sharktourshawaii', data)
--- tests/fareharbor_external_api_tests.py
+++ tests/fareharbor_external_api_tests.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 
 def fun(x):
     return x + 1
