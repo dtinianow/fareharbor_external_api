283ca341f1cac5ee0db4538dff4324c914278f0c
David Tinianow <david.tinianow@gmail.com>
2016-09-17 18:55:44 -0600

    Refactor service to be dynamic

--- fareharbor_external_api/fh.py
+++ fareharbor_external_api/fh.py
@@ -3,13 +3,14 @@ import os
 
 class Service:
 
-    def __init__(self):
-        api_app_key = os.environ['FAREHARBOR_API_APP_KEY']
-        api_user_key = os.environ['FAREHARBOR_API_USER_KEY']
-        self.request = requests.get("https://demo.fareharbor.com/api/external/v1/companies/?api-app=%s&api-user=%s" % (api_app_key, api_user_key))
+    url = 'https://demo.fareharbor.com/api/external/v1/'
+    api_app_key = os.environ['FAREHARBOR_API_APP_KEY']
+    api_user_key = os.environ['FAREHARBOR_API_USER_KEY']
+    payload = {'api-app': api_app_key, 'api-user': api_user_key}
 
     def get_companies(self):
-        print self.request.text
+        request = requests.get("%scompanies/?api-app=%s&api-user=%s" % (self.url, self.api_app_key, self.api_user_key))
+        print request.text
 
 x = Service()
 x.get_companies()
