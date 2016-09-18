491b16a7ef6c92aa4ed6736213c44f9b1a559cb4
David Tinianow <david.tinianow@gmail.com>
2016-09-17 18:34:11 -0600

    Refactor companies get request into class and methods

--- fareharbor_external_api/fh.py
+++ fareharbor_external_api/fh.py
@@ -1,8 +1,15 @@
 import requests
 import os
 
-api_app_key = os.environ['FAREHARBOR_API_APP_KEY']
-api_user_key = os.environ['FAREHARBOR_API_USER_KEY']
-r = requests.get("https://demo.fareharbor.com/api/external/v1/companies/?api-app=%s&api-user=%s" % (api_app_key, api_user_key))
+class Service:
 
-print r.text
+    def __init__(self):
+        api_app_key = os.environ['FAREHARBOR_API_APP_KEY']
+        api_user_key = os.environ['FAREHARBOR_API_USER_KEY']
+        self.request = requests.get("https://demo.fareharbor.com/api/external/v1/companies/?api-app=%s&api-user=%s" % (api_app_key, api_user_key))
+
+    def get_companies(self):
+        print self.request.text
+
+x = Service()
+x.get_companies()
