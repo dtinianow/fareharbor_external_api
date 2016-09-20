72bfc1d9f25395b97e799a5829da80bb749f41bc
Zack Forbing <zackforbing@gmail.com>
2016-09-19 18:19:26 -0600

    company class mostly complete

--- fareharbor/item.py
+++ fareharbor/item.py
@@ -1,5 +1,4 @@
 class Item:
     def __init__(self, params):
-        import code; code.interact(local=dict(globals(), **locals()))
         for key in params:
             setattr(self, key, params[key])
